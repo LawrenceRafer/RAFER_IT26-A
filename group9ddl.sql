20be499d57b15c180e0d92e75bc64d9faa9e8cb2
LawrenceRafer <20231858@nbsc.edu.ph>
2025-04-11 07:20:31 +0800

    group9ddl.sql

--- group9ddl.sql
+++ group9ddl.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `group3ddl`
+-- Database: `group9ddl`
 --
 
 -- --------------------------------------------------------
